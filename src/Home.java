2aaabe6e19e65930d1def11280b43b447b812e79
LukeyHikari <lukerosetepons@gmail.com>
2024-10-13 18:04:01 +0800

    Finalize output

--- src/Home.java
+++ src/Home.java
@@ -1,41 +1,54 @@
-public class Home{
-    String HomeId, address, status;
-    float price, area;
-    int bathNo, bedNo;
+import java.text.DecimalFormat;
 
+public class Home{
+    //Fields
+    String HomeId, address, status, bedAndBath;
+    double price, area;
+
+    //Create double precision decimal format for display
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+
+    //Constructor
     public Home(String HomeId, String address, String status,
-                float price, float area, int bathNo, int bedNo){
+                double price, double area, String bedAndBath) {
         this.HomeId = HomeId;
         this.address = address;
         this.status = status;
         this.price = price;
         this.area = area;
-        this.bathNo = bathNo;
-        this.bedNo = bedNo;
+        this.bedAndBath = bedAndBath;
     }
 
+    //Method for Object Creation
     public static Home newListing(String HomeId, String address,
-                                  String status, float price,
-                                  float area, int bathNo, int bedNo){
-        System.out.println("Adding Listing " + HomeId);
-        return new Home(HomeId, address, status, price, area, bathNo, bedNo);
+                                  String status, double price,
+                                  double area, String bedAndBath) {
+        return new Home(HomeId, address, status, price, area, bedAndBath);
     }
 
+    //Display Method
     public void displayListing(){
-        System.out.println("Displaying HomeId " + HomeId);
-        System.out.println("Address: " + address);
-        System.out.println("Price: " + price + " Status: " + status);
-        System.out.println("Area: " + area + "sqm");
-        System.out.println(bedNo + " BR " + bathNo + " Baths");
+        System.out.println("Displaying details of " + HomeId);
+        System.out.println("\tAddress: " + address);
+        System.out.println("\tPrice: Php " + df.format(price));
+        System.out.println("\tStatus: " + status);
+        System.out.println("\tArea: " + df.format(area) + "sqm");
+        System.out.println("\tRooms: " + bedAndBath + "\n");
     }
 
-    public void updatePrice(int price){
-        System.out.println("Updating Price " + price);
+    //Price Update Method
+    public void updatePrice(double price){
+        System.out.println("------------------------------");
+        System.out.println("Updating Price of " + HomeId + "... " + "Php " + df.format(price));
         this.price = price;
+        System.out.println("------------------------------" + "\n");
     }
 
+    //Status Update Method
     public void updateStatus(String status){
-        System.out.println("Updating Status " + status);
+        System.out.println("------------------------------");
+        System.out.println("Updating Status of " + HomeId + "... " + status);
         this.status = status;
+        System.out.println("------------------------------" + "\n");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        Home home1 = Home.newListing("2ba2be01",
-                "1 Main St., LA", "Listed",
-                100000, 36, 2,2);
-/*        Home home2 = Home.newListing("2ba2be01",
-                "1 Main St., LA", "Listed",
-                100000, 36, 2,2);
-        Home home3 = Home.newListing("2ba2be01",
-                "1 Main St., LA", "Listed",
-                100000, 36, 2,2);*/
-        home1.displayListing();
+        //Create Simple ArrayList to Store All Listings
+        ArrayList<Home> homes = new ArrayList<>();
+
+        //Create Home Listings
+        Home home1 = Home.newListing("Home1",
+                "1234 Mabini Street, Ermita, Manila, Metro Manila 1000", "Listed",
+                3500000, 36.59, "1 BR, 2 Baths");
+        Home home2 = Home.newListing("Home2",
+                "45 Sampaguita Avenue, Quezon City, Metro Manila 1101", "Listed",
+                5000000, 50.17, "3 BR, 2 Baths");
+        Home home3 = Home.newListing("Home3",
+                "House 16, Greenmeadows Subdivision, Quezon City, Metro Manila 1110", "Reserved",
+                3000000, 29.37, "1 BR, 1 Baths");
+
+        //Add all listings to the list
+        homes.add(home1);
+        homes.add(home2);
+        homes.add(home3);
+
+        //Display all the listings
+        for(int i = 0; i<3; i++) homes.get(i).displayListing();
+
+        //Update listing
+        home2.updatePrice(4500000);
+
+        //Display the updated listing
+        home2.displayListing();
+
+        //Update listing
+        home3.updateStatus("Sold");
+
+        //Display the updated listing
+        home3.displayListing();
     }
 }
 
